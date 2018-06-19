6a118208288e4a25f9dda5abd5672b47e04a6d17
zoripong <dbfl0406@e-mirim.hs.kr>
2018-06-20 01:26:30 +0900

    index, signin, user-index

--- include/script/index.js
+++ include/script/index.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-    $("#sc-logo").click(function() {
+    $(window).click(function() {
         $("#sc-login").css({
             "opacity": "1",
+            "width" : "35%",
             "height": "75%"
         });
-
+        $(body).css("cursor", "context-menu");
+        $(html).css("cursor", "context-menu");
+        $("#sc-logo").css("display", "none");
     });
+    $(body).css("cursor", "pointer");
+
 });
