6b2a4dd838282d92a90336e73e2eb5641b661b40
zoripong <dbfl0406@e-mirim.hs.kr>
2018-08-29 12:06:11 +0900

    image upload

--- include/script/getData.js
+++ include/script/getData.js
@@ -21,13 +21,13 @@ function getUserInfo() {
                     'user_id': user_list[i].user_id,
                     'password': user_list[i].password,
                     'nick_name': user_list[i].nick_name,
-                    'camera': JSON.parse(JSON.stringify(user_list[i].camera)) 
+                    'camera': JSON.parse(JSON.stringify(user_list[i].camera))
                 });
 
             }
 
             isLoading = false;
-            $('.loader').removeClass('loader');
+            $('#loader').removeClass('loader');
         }
     });
 }
@@ -42,22 +42,22 @@ function getBoard() {
         success: function(response, status, result) {
             var response = JSON.parse(JSON.stringify(response));
             var board_list = JSON.parse(JSON.stringify(response.board));
-
+            board_array = [];
             for (var i = 0; i < board_list.length; i++) {
 
                 var id = board_list[i].id;
-                if(sessionStorage.getItem("user_id") == board_list[i].user_id){
+                if (sessionStorage.getItem("user_id") == board_list[i].user_id) {
                     board_array.push({
                         'id': board_list[i].id,
                         'user_id': board_list[i].user_id,
                         'image_url': board_list[i].image_url,
                         'location': board_list[i].location,
-                        'description' : board_list[i].description,
-                        'camera' : board_list[i].camera,
-                        'angle' : board_list[i].angle,
-                        'tightening' : board_list[i].tightening,
-                        'shutter_speed' : board_list[i].shutter_speed,
-                        'iso' : board_list[i].iso,
+                        'description': board_list[i].description,
+                        'camera': board_list[i].camera,
+                        'angle': board_list[i].angle,
+                        'tightening': board_list[i].tightening,
+                        'shutter_speed': board_list[i].shutter_speed,
+                        'iso': board_list[i].iso,
                         'tag': JSON.parse(JSON.stringify(board_list[i].tag))
                     });
                 }
@@ -70,28 +70,29 @@ function getBoard() {
 }
 
 
-function setView(){
-    isLoading = false;
-    $('.loader').removeClass('loader');
-    // alert("hi"+board_array.length);
-    var str = "<section id='sc_user_images_row'>";
+function setView() {
 
-    for(var i = 0; i< board_array.length; i++){
-        if((i+1)%4 == 0){
-            str += "</section><section id='sc_user_images_row'>";
+    isLoading = false;
+    $('#loader').removeClass('loader');
+    // alert("hi"+board_array.length);
+    var str = "";
+    for (var i = 0; i < board_array.length; i++) {
+        if (i % 3 == 0) {
+            str += "<section id='sc_user_images_row'>";
         }
 
-
-        str += "<div class='user_image_container' onclick='openModal(\"include/image/image_1.jpg\", "+i+")'>";
-        str += "<img src='include/image/image_1.jpg'>";
+        str += "<div class='user_image_container' onclick='openModal(\"" + board_array[i].image_url + "\", " + i + ")'>";
+        str += "<img src='" + board_array[i].image_url + "'>";
         str += "<div class='image_opacity'></div>";
         str += "<div class='image_detail'>";
         str += "<img class='image_detail_btn' src='include/image/sc_loupe.svg'>";
         str += "</div>";
         str += "</div>";
+        if (i % 3 == 2) {
+            str += "</section>";
+        }
     }
-    str += "</section>";
     $("#sc_user_images").append(str);
-    
+
 
 }
--- include/script/main.js
+++ include/script/main.js
@@ -1,46 +1,47 @@
 var parentHeight = parseInt($(window).height());
+var imageFile = "";
 
-function openModal(path, id){
+function openModal(path, id) {
     $("#sc_popup_image>img").css({
-            "width": "auto",
-            "height": parentHeight - 250
-        });
-        
-        var popImageWidth = parseInt($("#sc_popup_image>img").width());
-        // alert(popImageWidth);
-        $("#sc_popup_image_background").css({
-            "background-image" : "url("+path+")",
-            "width": "100%",
-            "height": parentHeight - 250
-        });
-        $("#sc_popup_image>img").attr("src", path);
-        $("#sc_popup").css({
-            "opacity": "1.0",
-            "display" : "block"
-            });
+        "width": "auto",
+        "height": parentHeight - 250
+    });
 
-        $("#sc_popup_content").css({
-            "visibility": "visible",
-            "-webkit-transform": "scale(1, 1)",
-            "-ms-transform" : "scale(1, 1)",
-            "transform": "scale(1, 1)"
-        });
+    var popImageWidth = parseInt($("#sc_popup_image>img").width());
+    // alert(popImageWidth);
+    $("#sc_popup_image_background").css({
+        "background-image": "url(" + path + ")",
+        "width": "100%",
+        "height": parentHeight - 250
+    });
+    $("#sc_popup_image>img").attr("src", path);
+    $("#sc_popup").css({
+        "opacity": "1.0",
+        "display": "block"
+    });
 
-        var tags = "";
-        for(var i = 0; i<board_array[id].tag.length; i++){
-            tags += "<a href='#'><p>#"+board_array[id].tag[i]+"</p></a>";
-        }
-        $("#detail_tags").html(tags);
-        // alert(board_array[id].tag[0]);
-        p_location
-        $("#p_location").text(board_array[id].location);
-        $("#p_detail_explan").text(board_array[id].description);
-        $("#info_camera").text(board_array[id].camera);
+    $("#sc_popup_content").css({
+        "visibility": "visible",
+        "-webkit-transform": "scale(1, 1)",
+        "-ms-transform": "scale(1, 1)",
+        "transform": "scale(1, 1)"
+    });
 
-        $("#info_angle").text(board_array[id].angle);
-        $("#info_tightening").text(board_array[id].tightening);
-        $("#info_shutter_speed").text(board_array[id].shutter_speed);
-        $("#info_iso").text(board_array[id].iso);
+    var tags = "";
+    for (var i = 0; i < board_array[id].tag.length; i++) {
+        tags += "<a href='#'><p>#" + board_array[id].tag[i] + "</p></a>";
+    }
+    $("#detail_tags").html(tags);
+    // alert(board_array[id].tag[0]);
+    p_location
+    $("#p_location").text(board_array[id].location);
+    $("#p_detail_explan").text(board_array[id].description);
+    $("#info_camera").text(board_array[id].camera);
+
+    $("#info_angle").text(board_array[id].angle);
+    $("#info_tightening").text(board_array[id].tightening);
+    $("#info_shutter_speed").text(board_array[id].shutter_speed);
+    $("#info_iso").text(board_array[id].iso);
 }
 $(window).scroll(function() {
     var standardHeightPx = parentHeight * (35 / 100);
@@ -64,32 +65,35 @@ $(window).scroll(function() {
 
 $(window).on('load', function() {
     // var index = sessionStorage.getItem("user_index");
-    
-    if(sessionStorage.getItem("user_index")==null){
-        location.href="index.html";
+
+    if (sessionStorage.getItem("user_index") == null) {
+        location.href = "index.html";
     }
 
     getBoard();
 
     $("#p_user_name").text(sessionStorage.getItem("user_id"));
 
-    $("#add_board_form").submit(function(){
-       insertBoard(); 
-       return false;
+    $("#add_board_form").submit(function() {
+
+        uploadImage();
+        // insertBoard();
+        // alert(imageFile);
+        return false;
     });
 
     $("#close_modal").click(function() {
-        
+
         $("#sc_popup").css({
             "opacity": "0.0",
-            "display" : "none"
+            "display": "none"
         });
 
         $("#sc_popup_content").css({
-            "-webkit-transition" : "all 1s ease",
+            "-webkit-transition": "all 1s ease",
             "visibility": "collapse",
             "-webkit-transform": "scale(0, 0)",
-            "-ms-transform" : "scale(0, 0)",
+            "-ms-transform": "scale(0, 0)",
             "transform": "scale(0, 0)"
         });
     });
@@ -97,34 +101,34 @@ $(window).on('load', function() {
     $("#close_add_modal").click(function() {
         $("#sc_add_popup").css({
             "opacity": "0.0",
-            "display" : "none"    
+            "display": "none"
         });
-        
+
         $("#sc_add_popup_content").css({
-            "-webkit-transition" : "all 1s ease",
+            "-webkit-transition": "all 1s ease",
             "visibility": "collapse",
             "-webkit-transform": "scale(0, 0)",
-            "-ms-transform" : "scale(0, 0)",
+            "-ms-transform": "scale(0, 0)",
             "transform": "scale(0, 0)"
         });
     });
-    
+
     $("#sc_float_btn").click(function() {
         // alert("hellow");
         $("#sc_popup_add_image").css({
             "width": "100%",
             "height": parentHeight - 250
         });
-        
+
         $("#sc_add_popup").css({
             "opacity": "1.0",
-            "display" : "block"
+            "display": "block"
         });
-        
+
         $("#sc_add_popup_content").css({
             "visibility": "visible",
             "-webkit-transform": "scale(1, 1)",
-            "-ms-transform" : "scale(1, 1)",
+            "-ms-transform": "scale(1, 1)",
             "transform": "scale(1, 1)"
         });
     });
@@ -138,8 +142,13 @@ $(window).on('load', function() {
         e.stopPropagation();
     });
 
-    $("#input_image_upload").on("change", function(e) {
 
+    // let storageRef = firebase.storage().ref('photos/' + $("#input_image_upload").val());
+    // let fileUpload = document.getElementById("cameraInput")
+
+
+    $("#input_image_upload").on("change", function(e) {
+        imageFile = e.target.files[0];
         if (this.files && this.files[0]) {
             var reader = new FileReader();
 
@@ -181,21 +190,21 @@ $(document).keyup(function(e) {
             "opacity": "0.0",
             "display": "none"
         });
-        
+
         $("#sc_add_popup").css({
-            "opacity":"0.0",
-            "display":"none"
+            "opacity": "0.0",
+            "display": "none"
         });
-        
+
         $("#sc_popup_content").css({
-            "-webkit-transition" : "all 1s ease",
+            "-webkit-transition": "all 1s ease",
             "visibility": "collapse",
             "-webkit-transform": "scale(0, 0)",
             "-ms-transform": "scale(0, 0)",
             "transform": "scale(0, 0)"
         });
         $("#sc_add_popup_content").css({
-            "-webkit-transition" : "all 1s ease",
+            "-webkit-transition": "all 1s ease",
             "visibility": "collapse",
             "-webkit-transform": "scale(0, 0)",
             "-ms-transform": "scale(0, 0)",
@@ -204,11 +213,74 @@ $(document).keyup(function(e) {
     }
 });
 
-function insertBoard(){
+function uploadImage() {
+    $('#loader').addClass('loader');
+
+    var storageRef = firebase.storage().ref();
+    let file = imageFile; // upload the first file only
+    // File or Blob named mountains.jpg
+    // var file = ;
+
+    // Create the file metadata
+    var metadata = {
+        contentType: 'image/jpeg'
+    };
+
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
+        },
+        function(error) {
+
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+                case 'storage/unauthorized':
+                    // User doesn't have permission to access the object
+                    alert("권한이 없습니다.");
+                    break;
+                case 'storage/canceled':
+                    // User canceled the upload
+                    alert("취소 되었습니다.");
+                    break;
+                case 'storage/unknown':
+                    // Unknown error occurred, inspect error.serverResponse
+                    alert("오류 발생.");
+                    break;
+            }
+        },
+        function() {
+            // Upload completed successfully, now we can get the download URL
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log('File available at', downloadURL);
+                insertBoard(downloadURL);
+                // alert(downloadURL);
+            });
+        });
+}
+
+function insertBoard(img) {
     var tag_json = JSON.stringify($("#input_tag").val().split("#"));
     // alert(camera_json);
-    alert($("#info_camera").text());
-
+    // alert($("#input_info_camera").val())
+    // alert($("#input_info_angle").val());
+    // alert($("#input_info_tightening").text());
+    // alert($("#input_info_shutter_speed").text());
+    // alert($("#info_iso").text());
     $.ajax({
         type: "POST",
         async: true,
@@ -217,18 +289,32 @@ function insertBoard(){
         data: {
             // notice
             "user_id": sessionStorage.getItem("user_id"),
-            "image_url":$('#input_image_upload').val() ,
+            "image_url": img,
             "location": $("#location").val(),
             "description": $("#description").val(),
-            "camera": $("#info_camera").text(),
-            "angle": $("#info_angle").val(),
-            "tightening": $("#info_tightening").val(),
-            "shutter_speed": $("#info_shutter_speed").val(),
-            "iso": $("#info_iso").val(),
+            "camera": $("#input_info_camera").val(),
+            "angle": $("#input_info_angle").val(),
+            "tightening": $("#input_info_tightening").val(),
+            "shutter_speed": $("#input_info_shutter_speed").val(),
+            "iso": $("#input_info_iso").val(),
             "tag": tag_json
         },
         success: function(response, status, result) {
             alert("게시글 등록에 성공하였습니다." + response);
+            document.getElementById("sc_user_images").innerHTML = "";
+            $("#sc_add_popup").css({
+                "opacity": "0.0",
+                "display": "none"
+            });
+
+            $("#sc_add_popup_content").css({
+                "-webkit-transition": "all 1s ease",
+                "visibility": "collapse",
+                "-webkit-transform": "scale(0, 0)",
+                "-ms-transform": "scale(0, 0)",
+                "transform": "scale(0, 0)"
+            });
+            getBoard();
         },
         error: function(xhr, status, error) {
             var errorMessage = xhr.status + ': ' + xhr.statusText
@@ -237,5 +323,3 @@ function insertBoard(){
     });
 
 }
-
-
